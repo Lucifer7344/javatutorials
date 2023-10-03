1850072bf42ec741af710a9c759620ce11fe12ff
lucifer <luciferthedevil7344@gmail.com>
2023-10-03 20:44:29 +0530

    variable.class

--- vriables.java
+++ vriables.java
@@ -1,5 +1,11 @@
 package javatutorials;
 
-public class vriables {
-    
+class vriables {
+    public static void main (String arr[]){
+    System.out.println("hello world");
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+    }
 }
