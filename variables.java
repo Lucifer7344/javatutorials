e0388689c5cd4f55a726eeb784047a885f56890b
lucifer <luciferthedevil7344@gmail.com>
2023-10-03 21:10:20 +0530

    variables.java

---
+++ variables.java
@@ -0,0 +1,22 @@
+package javatutorials;
+
+class variables {
+    public static void main (String arr[]){
+        String  name= "tony stark";
+        int age =30;
+        int a = 23;
+        int b =34;
+        double price=25.30;
+        b=20;
+        name ="ironman";
+            System.out.println(name);
+             System.out.println(age);
+              System.out.println(a);
+              System.out.println(b);
+              System.out.println(price);
+              System.out.println(b);
+              System.out.println(name);
+            
+
+    }
+}
