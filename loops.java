c2a951202db7f259c5889d490dd4c16d95d0654d
lucifer <luciferthedevil7344@gmail.com>
2023-10-04 22:34:12 +0530

    loops

---
+++ loops.java
@@ -0,0 +1,9 @@
+package javatutorials;
+
+public class loops {
+    public static void main(String arr[]){
+        for (int counter = 0; counter <100; counter = counter+1 ){
+            System.out.println("hello world");
+        }
+    }
+}
