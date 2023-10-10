91ba2aa959a4a8a2b74938be32c530ac56532684
lucifer <luciferthedevil7344@gmail.com>
2023-10-10 10:34:39 +0530

    numericals

---
+++ numer.java
@@ -0,0 +1,13 @@
+package javatutorials;
+
+public class numer {
+    public static void main (String arr[]){
+    
+        for (int i=0; i<4; i++){
+            for (int j=0; j<5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
